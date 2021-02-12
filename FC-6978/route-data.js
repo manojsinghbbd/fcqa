981f876b2ecdca118ebafbc3e07365f19e892f70
manojsinghbbd <76727981+manojsinghbbd@users.noreply.github.com>
2021-02-12 13:18:31 +0530

    added sorting

--- FC-6978/route-data.js
+++ FC-6978/route-data.js
@@ -1,93 +1,111 @@
 'use strict';
 
-registerRouteData('ec-settings', [{
-    stateName: 'generalConfig.credentials',
-    url: '/credentials/:credentialGroup/:credentialType'
-}, {
-    stateName: 'generalConfig.departments',
-    url: '/departments'
-}, {
-    stateName: 'generalConfig.driversLicense',
-    url: '/drivers-license'
-}, {
-    stateName: 'generalConfig.employmentGroups',
-    url: '/employment-groups'
-}, {
-    stateName: 'generalConfig',
-    url: '/general-config',
+registerRouteData('ec-forms', [{
+    stateName: 'formBuilder.builder',
+    url: '/builder?deactivate',
     $permissions: function $permissions(permissionSvc) {
-        return permissionSvc.isPermittedAction('listHeaders', 'view');
+        return permissionSvc.isPermittedAction('formTemplates', 'create') || permissionSvc.isPermittedAction('formTemplates', 'edit');
     }
 }, {
-    stateName: 'generalConfig.jobTypes.jobTypeDetail',
-    url: '/{jobTypeId:[0-9A-Za-z-]{36}}?action'
-}, {
-    stateName: 'generalConfig.jobTypes',
-    url: '/job-types'
-}, {
-    stateName: 'generalConfig.locations',
-    url: '/locations'
-}, {
-    stateName: 'generalConfig.positions',
-    url: '/positions'
-}, {
-    stateName: 'generalConfig.signatures',
-    url: '/signatures',
+    stateName: 'formBuilder',
+    url: '/form-builder/{formId:[0-9A-Za-z-]+}',
     $permissions: function $permissions(permissionSvc) {
-        return permissionSvc.isPermittedAction('signatures', 'manage');
+        return permissionSvc.isPermittedAction('formTemplates', 'create') || permissionSvc.isPermittedAction('formTemplates', 'edit') || permissionSvc.isPermittedAction('formInstances', 'send');
     }
 }, {
-    stateName: 'generalConfig.statusReasons',
-    url: '/status-reasons'
+    stateName: 'formBuilder.overlayBuilder',
+    url: '/overlay-builder',
+    $permissions: function $permissions(permissionSvc) {
+        return !!permissionSvc.permittedActions.activeUser.idmAdministrator || !!permissionSvc.permittedActions.formTemplates.buildOverlay;
+    }
+}, {
+    stateName: 'formBuilder.preview',
+    url: '/preview?returnTo&showPDFoverlay',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('formTemplates', 'view') || permissionSvc.isPermittedAction('formTemplates', 'edit' || permissionSvc.isPermittedAction('formTemplates', 'create'));
+    }
+}, {
+    stateName: 'formBuilder.settings',
+    url: '/settings',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('formTemplates', 'create') || permissionSvc.isPermittedAction('formTemplates', 'edit');
+    }
+}, {
+    stateName: 'formInstance',
+    url: '/form-instance/{formInstanceId:[0-9A-Za-z-]+}?from&returnUrl&target'
+}, {
+    stateName: 'formTemplates',
+    url: '/form-templates',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('formTemplates', 'create') || permissionSvc.isPermittedAction('formTemplates', 'edit') || permissionSvc.isPermittedAction('formTemplates', 'send') || permissionSvc.isPermittedAction('formTemplates', 'archive') || permissionSvc.isPermittedAction('packets', 'create') || permissionSvc.isPermittedAction('packets', 'edit') || permissionSvc.isPermittedAction('packets', 'send') || permissionSvc.isPermittedAction('packets', 'archive');
+    }
+}, {
+    stateName: 'formTracking',
+    url: '/form-tracking',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('formInstances', 'track');
+    }
+}, {
+    stateName: 'formTracking.templateSummaryDetails',
+    url: '/template-summary-details?{templateId:[0-9A-Za-z-]+}',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('formInstances', 'track');
+    }
+}, {
+    stateName: 'formTracking.templateSummary',
+    url: '/template-summary',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('formInstances', 'track');
+    }
+}, {
+    stateName: 'formTracking.viewBySubject',
+    url: '/view-by-subject',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('formInstances', 'track');
+    }
+}, {
+    stateName: 'myFormsFormInstance',
+    url: '/form-instance/{formInstanceId:[0-9A-Za-z-]+}?from&returnUrl'
+}, {
+    stateName: 'inbox',
+    url: '/inbox?activeTab',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('activeUser', 'userKey');
+    }
 }, {
     stateName: 'index',
-    url: '/'
-}, {
-    stateName: 'permissions.groups.all',
-    url: '/all'
-}, {
-    stateName: 'permissions.groups.detail',
-    url: '/{groupId:[0-9A-Za-z-]+}'
-}, {
-    stateName: 'permissions.groups.detail.groupMembers',
-    url: '/group-members'
-}, {
-    stateName: 'permissions.groups.detail.profileAndScope',
-    url: '/profile-and-scope'
-}, {
-    stateName: 'permissions.groups',
-    url: '/groups'
-}, {
-    stateName: 'permissions',
-    url: '/permissions',
+    url: '/',
     $permissions: function $permissions(permissionSvc) {
-        return permissionSvc.isPermittedAction('permissionProfiles', 'edit');
+        return permissionSvc.isPermittedAction('formTemplates', 'create') || permissionSvc.isPermittedAction('formTemplates', 'edit') || permissionSvc.isPermittedAction('formTemplates', 'send') || permissionSvc.isPermittedAction('formTemplates', 'archive') || permissionSvc.isPermittedAction('packets', 'create') || permissionSvc.isPermittedAction('packets', 'edit') || permissionSvc.isPermittedAction('packets', 'send') || permissionSvc.isPermittedAction('packets', 'archive');
     }
 }, {
-    stateName: 'permissions.profiles.all',
-    url: '/all'
-}, {
-    stateName: 'permissions.profiles.detail',
-    url: '/{profileId:[0-9A-Za-z-]{36}}'
-}, {
-    stateName: 'permissions.profiles.detail.tab',
-    url: '/{action:view|edit}/{tab:[0-9A-Za-z-]+}'
-}, {
-    stateName: 'permissions.profiles',
-    url: '/profiles'
-}, {
-    stateName: 'permissions.users.all',
-    url: '/all'
-}, {
-    stateName: 'permissions.users.detail',
-    url: '/{userId:[0-9A-Za-z-]{36}}'
-}, {
-    stateName: 'permissions.users',
-    url: '/users'
-}, {
-    stateName: 'userAccounts',
-    url: '/user-accounts',
+    stateName: 'my-forms',
+    url: '/my-forms?activeTab',
     $permissions: function $permissions(permissionSvc) {
-        return permissionSvc.isPermittedAction('users', 'view') || permissionSvc.isPermittedAction('employees', 'manageAccountAccess');
+        return permissionSvc.isPermittedAction('activeUser', 'userKey');
+    }
+}, {
+    stateName: 'packetBuilder.Builder',
+    url: '/builder',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('packets', 'create') || permissionSvc.isPermittedAction('packets', 'edit');
+    }
+}, {
+    stateName: 'packetBuilder',
+    url: '/packet-builder/{packetId:[0-9A-Za-z-]+}',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('packets', 'create') || permissionSvc.isPermittedAction('packets', 'edit') || permissionSvc.isPermittedAction('packetInstances', 'send');
+    }
+}, {
+    stateName: 'packetBuilder.Settings',
+    url: '/packet-builder/settings',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('packets', 'create') || permissionSvc.isPermittedAction('packets', 'edit');
+    }
+}, {
+    stateName: 'send',
+    url: '/send/{type:[A-Za-z]+}/{id:[0-9A-Za-z-]+}',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('formTemplates', 'send') || permissionSvc.isPermittedAction('packets', 'send');
     }
 }]);
